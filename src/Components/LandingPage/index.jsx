f77dfe63b7f5dee02cfd42130ce54dddae37f2e5
PatrickFM8 <121717113+PatrickFM8@users.noreply.github.com>
2023-09-24 16:18:35 +0200

    tablet templates

--- src/Components/LandingPage/index.jsx
+++ src/Components/LandingPage/index.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function LandingPage() {
   return (
     <div className="flex flex-col items-center lg:ml-72 mt-10 text-slate-600">
-<img src="logo.png" alt="Eneport Logo" className="rotate-logo" />      
+<img src="logo.png" width="120" height="120" alt="Eneport Logo" />     
 <h1 className="text-4xl font-bold mb-4">E N E P O R T</h1>
       <p className="text-xl mb-6 text-center">
       Mapping the Future of Charging Infrastructure. All Data. One Intuitive Platform.
@@ -18,11 +18,15 @@ function LandingPage() {
         <p className='text-sm mt-4'>The current buildout of charging infrastructure is not sufficient to meet the increasing demand of EV charging. Charging Point Operators lack a comprehensive tool to initiate charging infra projects faster and more cost-effective.</p>
       </div>
       
-      <div className="max-w-3xl flex flex-col p-8 mt-10">
-      <h2 class="text-left text-gray-700 text-3xl font-semibold">Filling the Data Void</h2>
+      <div className="max-w-3xl flex flex-col p-8 mt-10 relative">
+    <h2 className="text-left text-gray-700 text-3xl font-semibold">Filling the Data Void</h2>
+    <p className='text-sm mt-4'>The current process involves the manual gathering of data from many sources, and time-consuming on-site visitations to infrastructure.</p>
+    <img src="ipadfront.png" alt="iPad Mockup" className="relative z-0" />
+    <div className="pulsating-circle absolute top-2/4 left-1/3 transform -translate-x-1/2 -translate-y-1/2 w-8 h-8 bg-blue-500 rounded-full animate-pulse z-10"></div>
+    <div className="pulsating-circle absolute top-3/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-8 h-8 bg-blue-500 rounded-full animate-pulse z-10"></div>
 
-        <p className='text-sm mt-4'>The current process involves the manual gathering of data from many sources, and time-consuming on-site visitations to infrastructure.</p>
-      </div>
+</div>
+
       
       <div className="max-w-3xl flex flex-col p-8 mt-10">
       <h2 class="text-left text-gray-700 text-3xl font-semibold">Data Mapping with Eneport</h2>
@@ -41,17 +45,9 @@ function LandingPage() {
         <p className='text-sm mt-4'>Eneport gathers all necessary data, including grid operator info, substation location, ownership info, and much more, in one maps platform. It allows Charging Point Operators to seamlessly plan their charging infra projects, contact customers, and save time
 </p>
       </div>
-      <div class="flex space-x-8 bg-white p-8 shadow-md">
-    <div class="card flex flex-col items-center p-8 bg-white shadow-lg">
-        <h3 class="card-title font-semibold text-gray-800 mb-4">Challenges in Meeting the Surge of EV Charging Demands</h3>
-        <p class="card-description text-gray-600 mb-8">Eneport brings an innovative solution to the challenges faced in the EV charging industry. With a state-of-the-art mapping system, gather all necessary data in one intuitive platform.</p>
-       
-    </div>
-    <div class="card flex flex-col items-center p-8 bg-white shadow-lg">
-        <h3 class="card-title font-semibold text-gray-800 mb-4">Make it yours</h3>
-        <p class="card-description text-gray-600 mb-8">With Eneport's customizability, tailor the platform to fit your organization's needs, ensuring seamless integration and usability.</p>
- 
-    </div>
+  
+<div>
+  <img src="ipadmap.png" width="800" height="800"></img>
 </div>
 
     </div>
--- src/Components/MapDemo/index.jsx
+++ src/Components/MapDemo/index.jsx
@@ -1,17 +1,29 @@
 import React, { useState } from 'react';
 import { MapContainer, TileLayer, CircleMarker, Marker } from 'react-leaflet';
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
 function MapDemo() {
   const centerCoordinates = [59.3293, 18.0686];
   const zoomLevel = 13;
-  
   const [showPins, setShowPins] = useState(false);
 
+  const customIcon = new L.Icon({
+    iconUrl: '/images/marker-icon.png',
+    iconRetinaUrl: '/images/marker-icon-2x.png',
+    shadowUrl: '/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    tooltipAnchor: [16, -28],
+    shadowSize: [41, 41]
+  });
+
   return (
-    <div className="flex items-center justify-center h-screen lg:ml-72 z-40">
-      <div className="h-[80vh] w-[66vw]">
-        <MapContainer center={centerCoordinates} zoom={zoomLevel} className="h-[66vh] w-[66vw]">
+    <div className="flex items-center justify-center h-screen lg:ml-72 z-40 relative">
+      <img src="Frontclean.png" alt="iPad Mockup" className="relative z-0" width="800" height="570" />
+      <div className="rounded-xl pr-4 h-[74vh] w-[59vw] absolute z-10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 overflow-hidden">
+        <MapContainer center={centerCoordinates} zoom={zoomLevel} className="h-full w-full rounded-lg">
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -20,14 +32,14 @@ function MapDemo() {
           {
             showPins && (
               <>
-                <Marker position={[59.3293 + 0.002, 18.0686]} />
-                <Marker position={[59.3293 - 0.002, 18.0686]} />
+                <Marker position={[59.3293 + 0.002, 18.0686]} icon={customIcon} />
+                <Marker position={[59.3293 - 0.002, 18.0686]} icon={customIcon} />
               </>
             )
           }
         </MapContainer>
-        <button onClick={() => setShowPins(true)}>Drop Pins</button>
       </div>
+      <button onClick={() => setShowPins(true)} className="z-20 absolute bottom-4 right-4">Drop Pins</button>
     </div>
   );
 }
